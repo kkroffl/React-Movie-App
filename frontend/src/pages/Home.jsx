70d5cc3410fcc8f26dd2408eaef6be08a2f9e0c1
kkroffl <rajakarthick366@gmail.com>
2026-08-26 20:55:54 +0530

    Added Movie Contexts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -27,20 +27,21 @@ function Home() {
 
   const handleSearch = async (e) => {
     e.preventDefault();
+
     if (!searchQuery.trim()) return;
+    if (loading) return;
 
     setLoading(true);
-    setError(null);
     try {
       const searchResults = await searchMovies(searchQuery);
       setMovies(searchResults);
+      setError(null);
     } catch (err) {
       console.log(err);
       setError("Failed to search movies...");
     } finally {
       setLoading(false);
     }
-    setSearchQuery("");
   };
 
   return (
@@ -57,12 +58,15 @@ function Home() {
           Search
         </button>
       </form>
-      {loading && <p>Loading movies...</p>}
-      {error && <p>{error}</p>}
-      <div className="movies-grid">
-        {!loading &&
-          movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
-      </div>
+      {error && <div className="error-message">{error}</div>}
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {!loading &&
+            movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
+        </div>
+      )}
     </div>
   );
 }
